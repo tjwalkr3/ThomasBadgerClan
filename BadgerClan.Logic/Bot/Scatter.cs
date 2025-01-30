fcbaf3cefd307da2d91c6180a4033d096bf2412f
Thomas Jones1 <thomas.jones1@students.snow.edu>
2025-01-30 13:22:35 -0700

    Made it so that Units don't get stuck against the wall in scatter mode.

--- BadgerClan.Logic/Bot/Scatter.cs
+++ BadgerClan.Logic/Bot/Scatter.cs
@@ -5,7 +5,6 @@ public class Scatter : IBot
 {
     public Task<List<Move>> PlanMovesAsync(GameState state)
     {
-        // Return an empty list if my team is null
         var myteam = state.TeamList.FirstOrDefault(t => t.Id == state.CurrentTeamId);
         if (myteam is null)
             return Task.FromResult(new List<Move>());
@@ -15,12 +14,36 @@ public class Scatter : IBot
         {
             var allOtherPlayers = state.Units.Where(u => u != unit);
             var closest = allOtherPlayers.OrderBy(u => u.Location.Distance(unit.Location)).FirstOrDefault();
-            if (closest != null)
+            Coordinate? closestWall = GetClosestWall(unit.Location, state.Dimension);
+
+            if (closest != null && closestWall is not null)
             {
-                moves.Add(SharedMoves.StepAwayFromClosest(unit, closest, state));
+                if (unit.Location.Distance(closestWall) < 3)
+                {
+                    // Move away from the wall if too close
+                    var moveAwayFromWall = unit.Location.Away(closestWall);
+                    moves.Add(new Move(MoveType.Walk, unit.Id, moveAwayFromWall));
+                }
+                else
+                {
+                    // Move away from the closest player
+                    moves.Add(SharedMoves.StepAwayFromClosest(unit, closest, state));
+                }
             }
         }
         return Task.FromResult(moves);
     }
 
+    private Coordinate? GetClosestWall(Coordinate location, int boardSize)
+    {
+        var walls = new List<Coordinate>
+        {
+            new Coordinate(0, location.R), // Left wall
+            new Coordinate(boardSize - 1, location.R), // Right wall
+            new Coordinate(location.Q, 0), // Top wall
+            new Coordinate(location.Q, boardSize - 1) // Bottom wall
+        };
+
+        return walls.OrderBy(w => w.Distance(location)).FirstOrDefault();
+    }
 }
