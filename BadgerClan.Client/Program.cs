5d95e6b166b8de15361ea54b625544e248d993f8
Thomas Jones1 <thomas.jones1@students.snow.edu>
2025-02-12 01:31:57 -0700

    removed the http stuff from the API

--- BadgerClan.Client/Program.cs
+++ BadgerClan.Client/Program.cs
@@ -6,41 +6,19 @@ using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Retrieve Base64-encoded certificate from environment variables
-var certBase64 = Environment.GetEnvironmentVariable("CERTIFICATE_BASE64");
-
+// Configure kestrel to run using the right ports and protocols
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(5001, listenOptions =>
     {
         listenOptions.Protocols = HttpProtocols.Http1;
-        Console.WriteLine("? Plain HTTP server running on port 5001");
     });
 
-    if (!string.IsNullOrEmpty(certBase64))
+    options.ListenAnyIP(5000, listenOptions =>
     {
-        try
-        {
-            // Decode Base64 string into byte array
-            byte[] certBytes = Convert.FromBase64String(certBase64);
-
-            // Load certificate using ReadOnlySpan<byte>
-            var cert = X509CertificateLoader.LoadCertificate(certBytes.AsSpan());
-
-            // Configure Kestrel to use the certificate
-            options.ListenAnyIP(5000, listenOptions =>
-            {
-                listenOptions.UseHttps(cert);
-                listenOptions.Protocols = HttpProtocols.Http2;
-            });
-
-            Console.WriteLine("? SSL Certificate Loaded Successfully!");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"? Failed to load SSL certificate: {ex.Message}");
-        }
-    }
+        listenOptions.UseHttps();
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
 });
 
 builder.Services.AddLogging();
