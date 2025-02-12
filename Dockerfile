47605f058d34e23bf37d65a63613e5a31efc030e
Thomas Jones <tjwalkr3@gmail.com>
2025-02-12 01:51:21 -0700

    created certs and bundled them with the docker image

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,13 @@ RUN dotnet publish "BadgerClan.Client/BadgerClan.Client.csproj" -c Release -o pu
 FROM mcr.microsoft.com/dotnet/aspnet:9.0
 WORKDIR /App
 COPY --from=build /App/publish .
+
+# Copy the certificates into the container
+COPY ./dev.crt /App/certs/dev.crt
+COPY ./dev.key /App/certs/dev.key
+
+# Set environment variables to use the certificate (optional)
+ENV ASPNETCORE_Kestrel__Certificates__Default__Path=/App/certs/dev.crt
+ENV ASPNETCORE_Kestrel__Certificates__Default__KeyPath=/App/certs/dev.key
+
 ENTRYPOINT ["dotnet", "BadgerClan.Client.dll"]
--- spawn_clients.sh
+++ spawn_clients.sh
@@ -5,8 +5,6 @@ LOCATION="westus"
 IMAGE="tjwalkr3/badgerclan-client"
 REGISTRY_SERVER="index.docker.io"
 REGISTRY_USER="tjwalkr3"
-GRPC_PORT="5000"
-HTTP_PORT="5001"
 REGISTRY_PASS="dckr_pat_VzQLAIUDYuZM7LKuG-yz6X2Sa_Q"
 
 # Create resource group
@@ -21,28 +19,23 @@ do
     CLIENT_NAME="thomas-client-auto$i"
     DNS_LABEL="$CLIENT_NAME"
     echo "Creating client: $CLIENT_NAME"
-    
+
     az container create \
-      --resource-group $RESOURCE_GROUP \
-      --name $CLIENT_NAME \
-      --image $IMAGE \
-      --dns-name-label $DNS_LABEL \
-      --os-type Linux \
-      --cpu 1 \
-      --memory 1.5 \
-      --registry-login-server $REGISTRY_SERVER \
-      --registry-username $REGISTRY_USER \
-      --registry-password $REGISTRY_PASS \
-      --ports $GRPC_PORT $HTTP_PORT \
-      --environment-variables \
-          "KESTREL__ENDPOINTS__GRPC__URL=http://0.0.0.0:$GRPC_PORT" \
-          "KESTREL__ENDPOINTS__GRPC__PROTOCOLS=Http2" \
-          "KESTREL__ENDPOINTS__WEBAPI__URL=http://0.0.0.0:$HTTP_PORT" \
-          "KESTREL__ENDPOINTS__WEBAPI__PROTOCOLS=Http1" \
-      --output none
+        --resource-group $RESOURCE_GROUP \
+        --name $CLIENT_NAME \
+        --image $IMAGE \
+        --dns-name-label $DNS_LABEL \
+        --os-type Linux \
+        --cpu 1 \
+        --memory 1.5 \
+        --registry-login-server $REGISTRY_SERVER \
+        --registry-username $REGISTRY_USER \
+        --registry-password $REGISTRY_PASS \
+        --ports 5000 5001 \
+        --output none
     
-    echo "    gRPC= http://$DNS_LABEL.$LOCATION.azurecontainer.io:$GRPC_PORT"
-    echo "    HTTP= http://$DNS_LABEL.$LOCATION.azurecontainer.io:$HTTP_PORT"
+    echo "    gRPC= https://$DNS_LABEL.$LOCATION.azurecontainer.io:5000"
+    echo "    HTTP= http://$DNS_LABEL.$LOCATION.azurecontainer.io:5001"
 done
 
 # Allow user to delete the resource group
